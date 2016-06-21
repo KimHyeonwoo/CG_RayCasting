00443d59abe734e5616a565f1c95e3ba3938fdac
KimHyeonwoo <woo960131@naver.com>
2016-06-21 09:41:08 +0900

    Implement refraction for sphere, Add textures

--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@
 #include "light.h"
 #include <vector>
 
-#define SIZE 1000
-#define SUPERSAMPLE 1
+#define SIZE 300
+#define SUPERSAMPLE 3
+#define PI 3.141592
 
-Point camera (SIZE / 2, SIZE / 2, SIZE);
+Point camera (SIZE / 2, SIZE / 2, SIZE / 2);
 vector <Sphere> sphereList;
 vector <Triangle> triangleList;
 vector <Light> lightList;
@@ -19,6 +20,9 @@ vector <Light> lightList;
 Point raycast (Point source, Point target, int depth);
 void drawRect (Point p1, Point p2, Point p3, Point p4, Material mat);
 float shadowRay (Point hit, Point light);
+Point getAmbient (int minShapeIndex, Point hit);
+Point getDiffuse (int minShapeIndex, Point hit);
+Point getSpecular (int minShapeIndex, Point hit);
 
 Point BLACK (0, 0, 0);
 Point RED (1, 0, 0);
@@ -29,56 +33,66 @@ Point CYAN (0, 1, 1);
 Point MAGENTA (1, 0, 1);
 Point WHITE (1, 1, 1);
 
+BMP checker;
+BMP baseball;
+
 int main() {
   BMP out;
   out.SetSize(SIZE, SIZE);
 	out.SetBitDepth(32);
+  checker.ReadFromFile("texture.bmp");
+  baseball.ReadFromFile("baseball.bmp");
 
-  Point lightPos (500, -450, -500);
+  Point lightPos (0.5 * SIZE, -0.45 * SIZE, -0.5 * SIZE);
   Point lightColor (1, 1, 1);
   Light l1 (lightPos, lightColor);
   lightList.push_back (l1);
-  Point lightPos2 (250, 500, 0);
+  Point lightPos2 (0.25 * SIZE, 0.5 * SIZE, 0);
   Light l2 (lightPos2, lightColor);
   //lightList.push_back (l2);
 
-  Point o1 (500, 1550, -1500);
-  Point o2 (500, 750, -1500);
-  Point o3 (600, 650, -1500);
-  Material m1 (CYAN, CYAN, CYAN, 0, 0, 0.1, 0.4, 0.5, 50, 1);
-  Material m2 (GREEN, GREEN, GREEN, 0, 0, 0.1, 0.4, 0.5, 50, 1);
+  Point o1 (0.5 * SIZE, 1.55 * SIZE, -1.5 * SIZE);
+  //Point o1 (0.5 * SIZE, 0.75 * SIZE, -1.5 * SIZE);
+  Point o2 (0.5 * SIZE, 0.75 * SIZE, -1.5 * SIZE);
+  Point o3 (0.6 * SIZE, 0.65 * SIZE, -1.5 * SIZE);
+  Point o4 (0.1 * SIZE, 0.5 * SIZE, -1.2 * SIZE);
+  Material m1 (CYAN, CYAN, CYAN, 1.0, 1.0, 0.1, 0.4, 0.5, 50, 0.8);
+  Material m2 (WHITE, WHITE, WHITE, 0, 0, 0.1, 0.4, 0.5, 50, 1);
   Material m3 (BLUE, BLUE, BLUE, 0, 0, 0.1, 0.4, 0.5, 50, 1);
-  Sphere s1 (o1, 100, m1);
-  Sphere s2 (o2, 100, m2);
-  Sphere s3 (o3, 100, m3);
+  Material sphereMat (MAGENTA, MAGENTA, MAGENTA, 0, 0, 0.1, 0.4, 0.5, 50, 1);
+  Sphere s1 (o1, 0.1 * SIZE, sphereMat);
+  Sphere s2 (o2, 0.1 * SIZE, m2);
+  Sphere s3 (o3, 0.1 * SIZE, m3);
+  Sphere s4 (o4, 0.3 * SIZE, m1);
   sphereList.push_back(s1); 
+  sphereList.push_back(s4);
 //  sphereList.push_back(s2); 
 //  sphereList.push_back(s3); 
 
-  Point p1 (-700, -700, -2900);
-  Point p2 (1700, -700, -2900);
-  Point p3 (1700, 1700, -2900);
-  Point p4 (-700, 1700, -2900);
-  Point p5 (-700, -700, 1000);
-  Point p6 (1700, -700, 1000);
-  Point p7 (1700, 1700, 1000);
-  Point p8 (-700, 1700, 1000);
+  Point p1 (-0.7 * SIZE, -0.7 * SIZE, -2.9 * SIZE);
+  Point p2 (1.7 * SIZE, -0.7 * SIZE, -2.9 * SIZE);
+  Point p3 (1.7 * SIZE, 1.7 * SIZE, -2.9 * SIZE);
+  Point p4 (-0.7 * SIZE, 1.7 * SIZE, -2.9 * SIZE);
+  Point p5 (-0.7 * SIZE, -0.7 * SIZE, 1.0 * SIZE);
+  Point p6 (1.7 * SIZE, -0.7 * SIZE, 1.0 * SIZE);
+  Point p7 (1.7 * SIZE, 1.7 * SIZE, 1.0 * SIZE);
+  Point p8 (-0.7 * SIZE, 1.7 * SIZE, 1.0 * SIZE);
   Material m4 (WHITE, WHITE, WHITE, 0, 0, 0.1, 0.4, 0.5, 100, 1);
   Material m5 (WHITE, WHITE, WHITE, 0, 0.9, 0.1, 0.4, 0.5, 100, 1);
   drawRect (p1, p2, p3, p4, m4); // Front Face
-  drawRect (p1, p4, p8, p5, m4); // Top Face
-  drawRect (p3, p7, p8, p4, m4); // Right Face
-  drawRect (p2, p6, p7, p3, m4); // Bottom Face
-  drawRect (p1, p5, p6, p2, m4); // Left Face
+  drawRect (p1, p4, p8, p5, m3); // Top Face
+  drawRect (p3, p7, p8, p4, m2); // Right Face
+  drawRect (p2, p6, p7, p3, m3); // Bottom Face
+  drawRect (p1, p5, p6, p2, m2); // Left Face
   drawRect (p8, p7, p6, p5, m4); // Back Face
 
-  Point glass1 (500 - 1000 / sqrt(3), 450, -500);
-  Point glass2 (500, 450, -2000);
-  Point glass3 (500 + 1000 / sqrt(3), 450, -500);
-  Point glass4 (500 - 1000 / sqrt(3), 1700, -500);
-  Point glass5 (500, 1700, -2000);
-  Point glass6 (500 + 1000 / sqrt(3), 1700, -500);
-  Material glassMat (WHITE, WHITE, WHITE, 0, 1, 0.1, 0.4, 0.5, 100, 1);
+  Point glass1 (0.5 * SIZE - SIZE / sqrt(3), 1.1 * SIZE, -1.0 * SIZE);
+  Point glass2 (0.5 * SIZE, 1.1 * SIZE, -2.0 * SIZE);
+  Point glass3 (0.5 * SIZE + SIZE / sqrt(3), 1.1 * SIZE, -1.0 * SIZE);
+  Point glass4 (0.5 * SIZE - SIZE / sqrt(3), 1.7 * SIZE, -1.0 * SIZE);
+  Point glass5 (0.5 * SIZE, 1.7 * SIZE, -2.0 * SIZE);
+  Point glass6 (0.5 * SIZE + SIZE / sqrt(3), 1.7 * SIZE, -1.0 * SIZE);
+  Material glassMat (YELLOW, YELLOW, YELLOW, 0, 0.85, 0.1, 0.4, 0.5, 100, 1);
   drawRect (glass1, glass2, glass5, glass4, glassMat);
   drawRect (glass2, glass3, glass6, glass5, glassMat);
 
@@ -152,14 +166,17 @@ Point raycast(Point source, Point target, int depth) {
   Point viewNormal = (camera - hit).normalize();
   Material mat = minShapeIndex >= sphereList.size() ? triangleList[minShapeIndex - sphereList.size()].mat : sphereList[minShapeIndex].mat;
   
-  Point ret = mat.ambient * mat.ambientConst;
+//  Point ret = mat.ambient * mat.ambientConst;
+  Point ret = getAmbient (minShapeIndex, hit) * mat.ambientConst;
 
   for (int k = 0; k < lightList.size(); k++) {
     Point lightNormal = (lightList[k].position - hit).normalize();
-    Point diffuseLight = (normal * lightNormal) > 0 ? lightList[k].color.componentProduct (mat.diffuse) * mat.diffuseConst * (normal * lightNormal) : lightList[k].color * 0;
+    //Point diffuseLight = (normal * lightNormal) > 0 ? lightList[k].color.componentProduct (mat.diffuse) * mat.diffuseConst * (normal * lightNormal) : lightList[k].color * 0;
+    Point diffuseLight = (normal * lightNormal) > 0 ? lightList[k].color.componentProduct (getAmbient (minShapeIndex, hit)) * mat.diffuseConst * (normal * lightNormal) : lightList[k].color * 0;
     Point reflectNormal = normal * 2 * (lightNormal * normal) - lightNormal;
     // Point viewNormal = (camera - hit).normalize();
-    Point specularLight = lightList[k].color.componentProduct (mat.specular) * mat.specularConst * pow (reflectNormal * viewNormal, mat.gloss);
+    //Point specularLight = lightList[k].color.componentProduct (mat.specular) * mat.specularConst * pow (reflectNormal * viewNormal, mat.gloss);
+    Point specularLight = lightList[k].color.componentProduct (getAmbient (minShapeIndex, hit)) * mat.specularConst * pow (reflectNormal * viewNormal, mat.gloss);
     float rayContribution = shadowRay (hit, lightList[k].position);
     ret = ret + (diffuseLight + specularLight) * rayContribution;
     /*
@@ -170,49 +187,57 @@ Point raycast(Point source, Point target, int depth) {
     */
   }
 
-  Point l = hit - source;
+  // Reflect
+  Point l = (hit - source).normalize();
   Point reflectTarget = hit + l - normal * 2 * (l * normal);
   Point reflectLight = raycast (hit, reflectTarget, depth - 1);
+
+  // Refract
+  if (minShapeIndex < sphereList.size()) { // refract for sphere
+    if (mat.n * mat.n - 1 + (l * normal) * (l * normal) < 0) return ret * (1 - mat.beta) + reflectLight * mat.beta;
+    Point refractVector = l / mat.n - normal * ((l * normal) / mat.n + sqrt(mat.n * mat.n - 1 + (l * normal) * (l * normal)) / mat.n);
+    Point refractVector2 = refractVector * 2 * (l * refractVector) - l;
+    Point refractSource = hit - refractVector * 2 * ((hit - sphereList[minShapeIndex].origin) * refractVector);
+    Point refractTarget = refractSource + refractVector2;
+    Point refractLight = raycast (refractSource, refractTarget, depth - 1);
+    return ret * (1 - mat.alpha) * (1 - mat.beta) + reflectLight * (1 - mat.alpha) * mat.beta + refractLight * mat.alpha;
+  } else { // refract for triangle
+    return ret * (1 - mat.beta) + reflectLight * mat.beta;
+  }
 
   return ret * (1 - mat.alpha) * (1 - mat.beta) + reflectLight * (1 - mat.alpha) * mat.beta ;
   
 }
 
 float shadowRay (Point hit, Point light) {
-  Ray shadow (hit, light);
+  Point dx (SIZE / 20, 0, 0);
+  Point dz (0, 0, SIZE / 20);
   int count = 0;
-  float distance = (light - hit).length();
-  for (int i = 0; i < sphereList.size(); i++) {
-    Point tmp = sphereList[i].raycastHit (shadow, hit);
-    if (!(tmp == hit)) {
-      if ((tmp - hit).length() < distance) {
-        /*
-        cout << "sphere hit ";
-        hit.print();
-        cout << "sphere tmp ";
-        tmp.print();
-        cout << "sphere dist " << (tmp - hit).length() << endl;
-        */
-        count++;
+  for (int i = -1; i < 2; i++) {
+    for (int j = -1; j < 2; j++) {
+      Ray shadow (hit, light + dx * i + dz * j);
+      int tmpCount = 0;
+      float distance = (light + dx * i + dz * j - hit).length();
+      for (int k = 0; k < sphereList.size(); k++) {
+        Point tmp = sphereList[k].raycastHit (shadow, hit);
+        if (!(tmp == hit)) {
+          if ((tmp - hit).length() < distance) {
+            tmpCount++;
+          }
+        }
       }
+      for (int k = 0; k < triangleList.size(); k++) {
+        Point tmp = triangleList[k].raycastHit (shadow, hit);
+        if (!(tmp == hit)) {
+          if ((tmp - hit).length() < distance) {
+            tmpCount++;
+          }
+        }
+      }
+      count = tmpCount == 0 ? count + 1 : count;
     }
   }
-  for (int i = 0; i <triangleList.size(); i++) {
-    Point tmp = triangleList[i].raycastHit (shadow, hit);
-    if (!(tmp == hit)) {
-      if ((tmp - hit).length() < distance) {
-        /*
-        cout << "Triangle hit ";
-        hit.print();
-        cout << "Triangle tmp ";
-        tmp.print();
-        cout << "Triangle dist " << (tmp - hit).length() << endl;
-        */
-        count++;
-      }
-    }
-  }
-  return count == 0 ? 1.0 : 0.0;
+  return (double) count / 9.0;
 }
 
 void drawRect (Point p1, Point p2, Point p3, Point p4, Material mat) {
@@ -221,3 +246,27 @@ void drawRect (Point p1, Point p2, Point p3, Point p4, Material mat) {
   triangleList.push_back (t1);
   triangleList.push_back (t2);
 }
+
+Point getAmbient (int minShapeIndex, Point hit) {
+  if (minShapeIndex == 0) {
+    Point n = (hit - sphereList[0].origin).normalize();
+    float u = (atan2 (n.y, n.x) + PI) / (2 * PI);
+    float v = (atan2 (sqrt (pow (n.x, 2) + pow (n.y, 2)), n.z) / PI);
+    u = u > 0.2 ? u - 0.2 : u + 0.8;
+    v = v > 0.8 ? 1 : v * 1.25;
+    Point ret (baseball (u * 400, v * 200)->Red, baseball (u * 400, v * 200)->Green, baseball (u * 400, v * 200)->Blue);
+    return ret;
+  }
+  if (minShapeIndex == sphereList.size() || minShapeIndex == sphereList.size() + 1) {
+    Point u = triangleList[0].p3 - triangleList[0].p2;
+    Point v = triangleList[0].p1 - triangleList[0].p2;
+    Point w = hit - triangleList[0].p2;
+    float s1 = ((u * v) * (w * v) - (v * v) * (w * u)) / ((u * v) * (u * v) - (u * u) * (v * v));
+    float s2 = ((u * v) * (w * u) - (u * u) * (w * v)) / ((u * v) * (u * v) - (u * u) * (v * v));
+    Point ret (checker (s1 * 283, s2 * 283)->Red, checker (s1 * 283, s2 * 283)->Green, checker (s1 * 283, s2 * 283)->Blue);
+    return ret;
+  }
+  return minShapeIndex < sphereList.size() ? sphereList[minShapeIndex].mat.ambient : triangleList[minShapeIndex - sphereList.size()].mat.ambient;
+}
+
+
